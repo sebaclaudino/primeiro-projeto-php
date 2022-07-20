df5d543ab78e265702ebe7c3ce200ff33ca3647f
Sebastião <claudinosjr@hotmail.com>
2022-07-20 10:36:42 -0300

    Inclusão da página Index e exercícios de fixação 01 e 02

---
+++ ex01.php
@@ -0,0 +1,15 @@
+<html>
+	<head>
+		<title>Meu primeiro site em PHP!</title>
+	</head>
+	<body>
+		
+		<?php
+			for ( $i = 0; $i < 10 ; $i++ ){
+			print( "Linha número " .$i. "<br />");
+		}
+		?>
+		<br>
+		<a href="./index.php">Voltar ao índice</a><br>
+	</body>
+</html>
